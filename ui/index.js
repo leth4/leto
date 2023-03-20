0317051bdf68035e6d1c04f88e721debd7e716e5
lethargic <v34d4r@gmail.com>
2023-03-21 02:02:16 +0600

    open & save files, toggle spellcheck

--- ui/index.js
+++ ui/index.js
@@ -1,22 +1,70 @@
 const {appWindow} = window.__TAURI__.window;
-const {register} = window.__TAURI__.globalShortcut;
-const {readDir, BaseDirectory} = window.__TAURI__.fs;
+const {register, unregister} = window.__TAURI__.globalShortcut;
+const {writeTextFile, readTextFile, readDir, BaseDirectory} = window.__TAURI__.fs;
+const {open} = window.__TAURI__.dialog;
+const {appDir} = window.__TAURI__.path;
 
+await register('CmdOrControl+S', () => {saveSelectedFile()})
+await register('CmdOrControl+O', () => {openFile()})
+await register('CmdOrControl+]', () => {changeFontSize(3)})
+await register('CmdOrControl+[', () => {changeFontSize(-3)})
+//await register('CmdOrControl+Q', () => {toggleSpellcheck()})
 
-async function closewindow() {
-    await appWindow.close();
+document.addEventListener('keydown', function(event) {
+    if(event.key == 'Escape') { closewindow(); }
+});
+
+window.onkeydown = (e) => {
+  if (e.ctrlKey && (e.code === 'KeyQ')) {
+      e.preventDefault();
+      toggleSpellcheck();
+      return;
+  }
 }
 
-// await readDir('users', { dir: BaseDirectory.Desktop, recursive: true }).then(function showFiles(entries) {
+//document.getElementById("text-editor").addEventListener('input', saveSelectedFile(), false);
+
+var selectedFile;
+
+async function closewindow() { await appWindow.close(); }
+
+ 
+async function openFile() {
+    selectedFile = await open({
+        multiple: false,
+        filters: [{name: "", extensions: ['txt', 'md'] }]
+    });
+    openSelectedFile();
+}
+
+// async function readSelectedDir() {
+// await readDir(selected[0], {recursive: true }).then(function showFiles(entries) {
 // entries.forEach(element => {
 //         console.log(element);
 //     });
 // });
+// }
 
-//await register('CmdOrControl+Plus', () => {changeFontSize(3)})
+async function openSelectedFile() {
+    document.getElementById('text-editor').value = await readTextFile(selectedFile);
+}
+
+async function saveSelectedFile() {
+    if (selectedFile == null) return;
+    await writeTextFile(selectedFile, document.getElementById('text-editor').value);
+}
+
+
+
 
 function setTheme(theme) {
     document.getElementById("theme-link").setAttribute("href", `themes/${theme}.css`);
+}
+
+function toggleSpellcheck() {
+    var editor = document.getElementById("text-editor");
+    var newValue = editor.getAttribute("spellcheck") == "true" ? "false" : "true";
+    editor.setAttribute("spellcheck", newValue);
 }
 
 function changeFontSize(change) {
@@ -26,16 +74,3 @@ function changeFontSize(change) {
     r.style.setProperty('--font-size', `${num + change}px`);
 }
 
-
-
-document.addEventListener('keydown', function(event) {
-    if(event.key == 'Escape') {
-        closewindow();
-    }
-    if(event.ctrlKey && event.key=="-") {
-        changeFontSize(-3)
-    }
-    if(event.ctrlKey && event.key=="=") {
-        changeFontSize(3)
-    }
-});
